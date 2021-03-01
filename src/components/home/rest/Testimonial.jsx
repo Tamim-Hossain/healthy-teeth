ca8069aa5b58cdda6b4f45eacbf85128212511e6
tamim-hossain <tamim.cloud@gmail.com>
2021-03-01 17:22:19 +0600

    working..

--- src/components/home/rest/Testimonial.jsx
+++ src/components/home/rest/Testimonial.jsx
@@ -1,12 +1,54 @@
-import { Container } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
+import emma from "../../../images/emma.png";
+import lisa from "../../../images/lisa.png";
+import wilson from "../../../images/wilson.png";
 
 const Testimonial = () => {
+	const testimonialsData = [
+		{
+			comment:
+				"Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic non architecto nobis, adipisci recusandae repellat accusantium consequuntur, qui nisi deserunt blanditiis mollitia, illo! ",
+			name: "Wilson Harry",
+			city: "New York",
+			img: wilson,
+		},
+		{
+			comment:
+				"Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic non architecto nobis, adipisci recusandae repellat accusantium consequuntur, qui nisi deserunt blanditiis mollitia, illo! ",
+			name: "Emma Gomez",
+			city: "California",
+			img: emma,
+		},
+		{
+			comment:
+				"Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic non architecto nobis, adipisci recusandae repellat accusantium consequuntur, qui nisi deserunt blanditiis mollitia, illo! ",
+			name: "Lisa Farari",
+			city: "Brooklyn",
+			img: lisa,
+		},
+	];
 	return (
 		<Container>
 			<h5 className="text-info font-weight-bold">TESTIMONIAL</h5>
 			<h2 className="font-weight-bold mb-5">
 				What's Our Patients <br /> Says
 			</h2>
+			{testimonialsData.map((testimonial) => (
+				<Row>
+					<Col md={4} className='shadow'>
+						<p>{testimonial.comment}</p>
+						<div className="d-flex">
+							<div>
+								<img src={testimonial.img} alt="" className="img-fluid" />
+							</div>
+							<div>
+								<h6 className="text-info font-weight-bold">{testimonial.name}</h6>
+								<p className="text-secondary">{testimonial.city}</p>
+							</div>
+						</div>
+					</Col>
+				</Row>
+			))}
 		</Container>
 	);
 };
