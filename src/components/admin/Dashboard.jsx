0a181ef2e30995712f851f8158b17630d17abfff
tamim-hossain <tamim.cloud@gmail.com>
2021-03-02 15:59:13 +0600

    working on dashboard..

--- src/components/admin/Dashboard.jsx
+++ src/components/admin/Dashboard.jsx
@@ -1,5 +1,5 @@
 import { Col, Container, Row } from "react-bootstrap";
-import { Link, useLocation } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import Menu from "../shared/Menu";
 import AllAppointment from "./AllAppointment";
 import TodayAppointment from "./TodayAppointment";
@@ -13,8 +13,12 @@ const Dashboard = () => {
 			<Row>
 				<Menu />
 				<Col md={3}>
-					<Link to="/dashboard/today">Todays Appointments</Link>
-					<Link to="/dashboard/all">All Appointments</Link>
+					<NavLink activeClassName="bg-light" className="dropdown-item" to="/dashboard/today">
+						Todays Appointments
+					</NavLink>
+					<NavLink activeClassName="bg-light" className="dropdown-item" to="/dashboard/all">
+						All Appointments
+					</NavLink>
 				</Col>
 				<Col md={9}>
 					{pathname === "/dashboard" && <TodayAppointment />}
