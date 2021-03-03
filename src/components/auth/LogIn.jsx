900cb3095dcdbd3ce38f98b15dc7531c2da6a7e4
tamim-hossain <tamim.cloud@gmail.com>
2021-03-03 20:47:27 +0600

    working on admin page..

--- src/components/auth/LogIn.jsx
+++ src/components/auth/LogIn.jsx
@@ -1,10 +1,42 @@
-import { Container } from "react-bootstrap";
+import { Button, Container, Form } from "react-bootstrap";
+import { useForm } from "react-hook-form";
 import Menu from "../shared/Menu";
 
 const LogIn = () => {
+	const { register, handleSubmit, errors } = useForm(); // initialize the hook
+
+	const onSubmit = (data) => {
+		console.log(data);
+	};
 	return (
 		<Container>
 			<Menu />
+
+			<Form onSubmit={handleSubmit(onSubmit)}>
+				<Form.Group>
+					<Form.Label>Email</Form.Label>
+					<Form.Control
+						placeholder="Enter Email Address"
+						type="email"
+						name="email"
+						ref={register({ required: true })}
+					/>
+				</Form.Group>
+
+				<Form.Group>
+					<Form.Label>Password</Form.Label>
+					<Form.Control
+						placeholder="Enter Password"
+						type="password"
+						name="password"
+						ref={register({ required: true })}
+					/>
+				</Form.Group>
+
+				<Button variant="info" className="font-weight-bold" type="submit">
+					Create New Account
+				</Button>
+			</Form>
 		</Container>
 	);
 };
--- src/components/auth/SignUp.jsx
+++ src/components/auth/SignUp.jsx
@@ -1,5 +1,39 @@
+import { Button, Form } from "react-bootstrap";
+import { useForm } from "react-hook-form";
+
 const SignUp = () => {
-	return <div>new admin</div>;
+	const { register, handleSubmit, errors } = useForm(); // initialize the hook
+
+	const onSubmit = (data) => {
+		console.log(data);
+	};
+	return (
+		<Form onSubmit={handleSubmit(onSubmit)}>
+			<Form.Group>
+				<Form.Label>Email</Form.Label>
+				<Form.Control
+					placeholder="Enter Email Address"
+					type="email"
+					name="email"
+					ref={register({ required: true })}
+				/>
+			</Form.Group>
+
+			<Form.Group>
+				<Form.Label>Password</Form.Label>
+				<Form.Control
+					placeholder="Enter Password"
+					type="password"
+					name="password"
+					ref={register({ required: true })}
+				/>
+			</Form.Group>
+
+			<Button variant="info" className="font-weight-bold" type="submit">
+				Create New Account
+			</Button>
+		</Form>
+	);
 };
 
 export default SignUp;
