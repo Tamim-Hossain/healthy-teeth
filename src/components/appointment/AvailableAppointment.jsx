488ad280802adfb7c342a426132bbe32b6e9c888
tamim-hossain <tamim.cloud@gmail.com>
2021-03-04 14:21:07 +0600

    working on loading animation

--- src/components/appointment/AvailableAppointment.jsx
+++ src/components/appointment/AvailableAppointment.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { Button, Card, Col, Container, Row } from "react-bootstrap";
+import Loader from "react-loader-spinner";
 import AppointmentModal from "./AppointmentModal";
 
 const AvailableAppointment = ({ date }) => {
@@ -7,6 +8,7 @@ const AvailableAppointment = ({ date }) => {
 	const [title, setTitle] = useState("");
 	const [time, setTime] = useState("");
 	const [appointments, setAppointments] = useState([]);
+	const [loading, setLoading] = useState(true);
 
 	const openModal = () => {
 		setIsModalOpen(true);
@@ -19,45 +21,48 @@ const AvailableAppointment = ({ date }) => {
 	useEffect(() => {
 		fetch("https://teeth-healthy.herokuapp.com/availableappointments")
 			.then((res) => res.json())
-			.then((data) => setAppointments(data));
+			.then((data) => {
+				setAppointments(data);
+				setLoading(false);
+			});
 	}, [appointments]);
 	return (
 		<Container className="mt-5 pt-5 mb-5">
 			<h2 className="font-weight-bold text-info text-center">AvailAble Appointments on {date}</h2>
 			<hr className="w-50" />
-			<Row>
-				{appointments.map((appointment) => (
-					<Col md={4} className="mt-5">
-						<Card className="p-3 border-0 shadow">
-							<Card.Body className="text-center">
-								<Card.Title className="font-weight-bold text-info">{appointment.name}</Card.Title>
-								<Card.Text className="font-weight-bold">{appointment.visitingTime}</Card.Text>
-								<Card.Subtitle className="text-secondary">
-									<small>{appointment.seat} SEATS AVAILABLE</small>
-								</Card.Subtitle>
-								<Button
-									className="font-weight-bold mt-3"
-									variant="info"
-									onClick={() => {
-										openModal();
-										setTitle(appointment.name);
-										setTime(appointment.visitingTime);
-									}}
-								>
-									BOOK APPOINTMENT
-								</Button>
-							</Card.Body>
-						</Card>
-					</Col>
-				))}
-			</Row>
-			<AppointmentModal
-				isModalOpen={isModalOpen}
-				closeModal={closeModal}
-				title={title}
-				date={date}
-				time={time}
-			/>
+			{loading ? (
+				<div className=",-5 d-flex justify-content-center">
+					<Loader type="Puff" color="#1cc7c1" height={150} width={150} />
+				</div>
+			) : (
+				<Row>
+					{appointments.map((appointment) => (
+						<Col md={4} className="mt-5">
+							<Card className="p-3 border-0 shadow">
+								<Card.Body className="text-center">
+									<Card.Title className="font-weight-bold text-info">{appointment.name}</Card.Title>
+									<Card.Text className="font-weight-bold">{appointment.visitingTime}</Card.Text>
+									<Card.Subtitle className="text-secondary">
+										<small>{appointment.seat} SEATS AVAILABLE</small>
+									</Card.Subtitle>
+									<Button
+										className="font-weight-bold mt-3"
+										variant="info"
+										onClick={() => {
+											openModal();
+											setTitle(appointment.name);
+											setTime(appointment.visitingTime);
+										}}
+									>
+										BOOK APPOINTMENT
+									</Button>
+								</Card.Body>
+							</Card>
+						</Col>
+					))}
+				</Row>
+			)}
+			<AppointmentModal isModalOpen={isModalOpen} closeModal={closeModal} title={title} date={date} time={time} />
 		</Container>
 	);
 };
